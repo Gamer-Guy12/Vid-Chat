121b3d594181af2d6e4ffc52305e1f42090cc167
Varunan Sanjeepan <varunan.sanjeepan@outlook.com>
2023-10-01 20:50:17 -0700

    Just add a sorta test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,7 +5,9 @@ describe('My First Test', () => {
   })
 
   it('Visits the message chat', () => {
-
     cy.visit('/chat/message')
+    cy.get('.message').type("This is a bot message")
+    cy.get('.username').type('Test Bot')
+    cy.get('.submit').click()
   })
 })
